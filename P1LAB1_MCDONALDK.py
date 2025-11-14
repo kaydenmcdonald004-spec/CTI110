1d59207de9b2b6357f66c8c0d7b13a0dae4e34d3
kaydenmcdonald004-spec <kaydenmcdonald004@gmail.com>
2025-11-13 20:41:48 -0500

    Add files via upload

---
+++ P1LAB1_MCDONALDK.py
@@ -0,0 +1,9 @@
+# Kayden McDonald
+# 11/13/2025
+# P1LAB1
+# This program asks the user for their first and last name and then displays a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to this course!")
